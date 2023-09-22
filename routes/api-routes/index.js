1e464b7d32dbf830ffc136ad8888f6927bbfdfd3
Nweekley84 <nweekley27@gmail.com>
2023-09-22 10:37:03 -0700

    changed index.js in api-routes, renamed a thought to thoughts, removed dashes from the routes

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,10 +1,22 @@
-// Create a router instance
-const router = require('express').Router();
-// Import user and thought routes
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
-// Define endpoints for user and thought routes
-router.use('/user',userRoutes);
-router.use('/thought',thoughtRoutes);
-// Export the router
+// const router = require('express').Router();
+
+// const userRoutes = require('./user-routes');
+// const thoughtRoutes = require('./thought-routes');
+
+// router.use('/user',userRoutes);
+// router.use('/thought',thoughtRoutes);
+// module.exports = router;
+
+// Importing the Express router
+const router = require("express").Router();
+
+// Importing the routes for thoughts and users
+const thoughtRoutes = require("./thoughtRoutes");  // Importing routes related to thoughts
+const userRoutes = require("./userRoutes");        // Importing routes related to users
+
+// Using the routes for thoughts and users under their respective endpoints
+router.use("/thoughts", thoughtRoutes);  // Mounting thoughtRoutes under the "/thoughts" endpoint
+router.use("/users", userRoutes);        // Mounting userRoutes under the "/users" endpoint
+
+// Exporting the configured router
 module.exports = router;
